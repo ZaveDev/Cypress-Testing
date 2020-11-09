f766f74d351a74fa4a0c067ff4a81090b7103831
zavedev <zavedev1@gmail.com>
2020-11-09 13:44:37 -0500

    formatting

--- cypress/integration/form_tests.js
+++ cypress/integration/form_tests.js
@@ -1,96 +1,82 @@
 // write tests here
-describe('Inputs and submit button', () => {
-  
-  it('can navigate to this site', () => {
-    cy.visit('http://localhost:1234')
-    cy.url().should('include', 'localhost')
-  })
+describe("Inputs and submit button", () => {
+  it("can navigate to this site", () => {
+    cy.visit("http://localhost:1234");
+    cy.url().should("include", "localhost");
+  });
 
-  it('submit button is disabled', () => {
-    cy.get('#submitBtn').should('be.disabled')
-  })  
+  it("submit button is disabled", () => {
+    cy.get("#submitBtn").should("be.disabled");
+  });
 
-  it('can type a text for a new quote',()=>{
+  it("can type a text for a new quote", () => {
     cy.get('input[name="text"]')
-      .type('Have Fun!')
-      .should('have.value', 'Have Fun!')
-  })
+      .type("Have Fun!")
+      .should("have.value", "Have Fun!");
+  });
 
-  it('can type an author for a new quote', () => {
+  it("can type an author for a new quote", () => {
     cy.get('input[name="author"]')
-      .type('My new author')
-      .should('have.value', 'My new author')
-  })
-  
-  it('the submit button is not disabled anymore', () => {
-    cy.get('#submitBtn')
-      .should('not.be.disabled')
-    
+      .type("My new author")
+      .should("have.value", "My new author");
+  });
 
-  })
+  it("the submit button is not disabled anymore", () => {
+    cy.get("#submitBtn").should("not.be.disabled");
+  });
 
-  it('can cancel the new quote', () => {
-    cy.get('#cancelBtn').click()
+  it("can cancel the new quote", () => {
+    cy.get("#cancelBtn").click();
 
-    cy.get('input[name="text"]')
-    .should('have.value', '')
-  
-    cy.get('input[name="author"]')
-      .should('have.value', '')
-    
-    cy.get('#submitBtn')
-      .should('be.disabled')
-  })
-})
+    cy.get('input[name="text"]').should("have.value", "");
 
-describe('Adding a new quote', () => {
-  
-    it ('can navigate to the site', () =>{
-        cy.visit('http://localhost:1234') 
-    })
+    cy.get('input[name="author"]').should("have.value", "");
 
-    it('can submit a quote', () => {
-        cy.get('input[name="text"]')
-        .type('Have Fun!')
-        cy.get('input[name="author"]')
-        .type('Gabe')
-        cy.get('#submitBtn').click()
-        cy.get(".container").contains('Have Fun!')
-    })
+    cy.get("#submitBtn").should("be.disabled");
+  });
+});
 
-    it('can delete newly added quote', () => {
-      cy.contains('Have Fun!').siblings('button:nth-of-type(2)').click()
-      
-      cy.contains('Have Fun! (Gabe)').should('not.exist')
-    })    
-})
+describe("Adding a new quote", () => {
+  it("can navigate to the site", () => {
+    cy.visit("http://localhost:1234");
+  });
 
-describe('Editing a quote', ()=>{
-    it ('can navigate to the site', () =>{
-      cy.visit('http://localhost:1234') 
-  })
+  it("can submit a quote", () => {
+    cy.get('input[name="text"]').type("Have Fun!");
+    cy.get('input[name="author"]').type("Gabe");
+    cy.get("#submitBtn").click();
+    cy.get(".container").contains("Have Fun!");
+  });
 
-  it('can submit a quote', () => {
-      cy.get('input[name="text"]')
-      .type('Have Fun!')
-      cy.get('input[name="author"]')
-      .type('Gabe')
-      cy.get('#submitBtn').click()
-      cy.get(".container").contains('Have Fun!')
-  })
+  it("can delete newly added quote", () => {
+    cy.contains("Have Fun!").siblings("button:nth-of-type(2)").click();
 
-  it('click the edit button and submit changes', () => {
-    cy.contains('Have Fun!').siblings('button:nth-of-type(1)').click()
-    cy.get('input[name="text"]')
-    .type('Have')
-    cy.get('input[name="author"]')
-    .type('Goob')
-    cy.get('#submitBtn').click()
-    cy.get(".container").contains('Have')
-  })
-  it('can delete newly added quote', () => {
-    cy.contains('Have Fun!').siblings('button:nth-of-type(2)').click()
-    
-    cy.contains('Have Fun! (Gabe)').should('not.exist')
-  }) 
-})
+    cy.contains("Have Fun! (Gabe)").should("not.exist");
+  });
+});
+
+describe("Editing a quote", () => {
+  it("can navigate to the site", () => {
+    cy.visit("http://localhost:1234");
+  });
+
+  it("can submit a quote", () => {
+    cy.get('input[name="text"]').type("Have Fun!");
+    cy.get('input[name="author"]').type("Gabe");
+    cy.get("#submitBtn").click();
+    cy.get(".container").contains("Have Fun!");
+  });
+
+  it("click the edit button and submit changes", () => {
+    cy.contains("Have Fun!").siblings("button:nth-of-type(1)").click();
+    cy.get('input[name="text"]').type("Have");
+    cy.get('input[name="author"]').type("Goob");
+    cy.get("#submitBtn").click();
+    cy.get(".container").contains("Have");
+  });
+  it("can delete newly added quote", () => {
+    cy.contains("Have Fun!").siblings("button:nth-of-type(2)").click();
+
+    cy.contains("Have Fun! (Gabe)").should("not.exist");
+  });
+});
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,90 +1,103 @@
-import React, { useState, useEffect } from 'react'
-import Form from './Form'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import Form from "./Form";
+import axios from "axios";
 
-const quotesURL = 'http://localhost:3333/api/quotes'
+const quotesURL = "http://localhost:3333/api/quotes";
 
 const initialFormState = {
-  id: '',
-  text: '',
-  author: '',
-}
+  id: "",
+  text: "",
+  author: "",
+};
 
 export default function Container() {
   ////////////// STATES //////////////
   ////////////// STATES //////////////
   ////////////// STATES //////////////
-  const [quotes, setQuotes] = useState([])
-  const [formValues, setFormValues] = useState(initialFormState)
+  const [quotes, setQuotes] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormState);
 
   ////////////// NETWORK HELPERS //////////////
   ////////////// NETWORK HELPERS //////////////
   ////////////// NETWORK HELPERS //////////////
   const getQuotes = () => {
-    axios.get(quotesURL)
-      .then(response => {
-        setQuotes(response.data)
+    axios
+      .get(quotesURL)
+      .then((response) => {
+        setQuotes(response.data);
       })
-      .catch(handleError)
-  }
+      .catch(handleError);
+  };
 
   const postQuote = ({ text, author }) => {
-    axios.post(quotesURL, { text, author })
-      .then(res => setQuotes(quotes.concat(res.data)))
+    axios
+      .post(quotesURL, { text, author })
+      .then((res) => setQuotes(quotes.concat(res.data)))
       .catch(handleError)
-      .finally(resetForm)
-  }
+      .finally(resetForm);
+  };
 
   const putQuote = ({ id, text, author }) => {
-    axios.put(`${quotesURL}/${id}`, { text, author })
-      .then(res => {
-        setQuotes(quotes.map(quote => {
-          return quote.id === id ? res.data : quote
-        }))
+    axios
+      .put(`${quotesURL}/${id}`, { text, author })
+      .then((res) => {
+        setQuotes(
+          quotes.map((quote) => {
+            return quote.id === id ? res.data : quote;
+          })
+        );
       })
       .catch(handleError)
-      .finally(resetForm)
-  }
+      .finally(resetForm);
+  };
 
   const deleteQuote = (id) => {
-    axios.delete(`${quotesURL}/${id}`)
-      .then(res => { // eslint-disable-line
-        setQuotes(quotes.filter(quote => quote.id !== id))
+    axios
+      .delete(`${quotesURL}/${id}`)
+      .then((res) => {
+        // eslint-disable-line
+        setQuotes(quotes.filter((quote) => quote.id !== id));
       })
       .catch(handleError)
-      .finally(resetForm)
-  }
+      .finally(resetForm);
+  };
 
   ////////////// OTHER HELPERS //////////////
   ////////////// OTHER HELPERS //////////////
   ////////////// OTHER HELPERS //////////////
   const editQuote = (id) => {
-    const quote = quotes.find(q => q.id === id)
-    setFormValues({ ...quote })
-  }
+    const quote = quotes.find((q) => q.id === id);
+    setFormValues({ ...quote });
+  };
 
-  const handleError = err => { debugger } // eslint-disable-line
+  const handleError = (err) => {
+    debugger;
+  }; // eslint-disable-line
 
-  const resetForm = () => setFormValues(initialFormState)
+  const resetForm = () => setFormValues(initialFormState);
 
   ////////////// SIDE EFFECTS //////////////
   ////////////// SIDE EFFECTS //////////////
   ////////////// SIDE EFFECTS //////////////
-  useEffect(() => getQuotes(), [])
+  useEffect(() => getQuotes(), []);
 
   return (
-    <div className='container'>
+    <div className="container">
       <h3>Quotes</h3>
       <ul>
-        {
-          quotes.map((q, i) => (
-            <li key={q.id}>
-              <div>{q.text} ({q.author})</div>
-              <button data-cy={`editBtn${i}`} onClick={() => editQuote(q.id)}>Edit</button>
-              <button data-cy={`deleteBtn${i}`} onClick={() => deleteQuote(q.id)}>Delete</button>
-            </li>
-          ))
-        }
+        {quotes.map((q, i) => (
+          <li key={q.id}>
+            <div>
+              {q.text} ({q.author})
+            </div>
+            <button data-cy={`editBtn${i}`} onClick={() => editQuote(q.id)}>
+              Edit
+            </button>
+            <button data-cy={`deleteBtn${i}`} onClick={() => deleteQuote(q.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
       </ul>
       <Form
         values={formValues}
@@ -93,5 +106,5 @@ export default function Container() {
         reset={resetForm}
       />
     </div>
-  )
+  );
 }
